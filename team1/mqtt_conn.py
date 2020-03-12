f155ddbb6643df27e9758cde76f2edb71a53db48
Randy Lai YongHao <randylai.2016@sis.smu.edu.sg>
2020-03-12 13:27:18 +0800

    Update mqtt conn method

--- team1/mqtt_conn.py
+++ team1/mqtt_conn.py
@@ -1,10 +1,13 @@
 import paho.mqtt.client as mqtt
 
-def create_client():
-    client = mqtt.Client(client_id="Grp5")
+def create_client(c_id):
+    client = mqtt.Client(client_id=c_id)
     client.username_pw_set(username="iotcollabhuat", password="iott1t5")
     client.connect("18.141.11.6")
     return client
     
 def publish_motion_results(client,msg):
     client.publish("mr/motion",msg)
+    
+def publish_tof_results(client,msg):
+    client.publish("mr/tof",msg)
